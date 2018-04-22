00287db273025ba8ba216fe6e2c3aa74ccb83cc8
Thomas Brooks <tombrooks191@gmail.com>
2018-04-21 23:48:03 -0500

    initial commit

---
+++ GuerillaSharp.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GuerillaSharp.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GuerillaSharp.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("31d6f061-482f-4bb3-a8df-c659533ecdf6")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ GuerrillaSharp/GuerrillaSharp.cs
@@ -0,0 +1,76 @@
+﻿using GuerrillaSharp.Models;
+using GuerrillaSharp.Util;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuerrillaSharp
+{
+    public class GuerrillaMail
+    {
+        private CookieContainer Cookies { get; set; }
+
+        [JsonProperty("sid_token")]
+        private string SidToken { get; set; }
+
+        [JsonProperty("email_addr")]
+        public string EmailAddress { get; set; }
+
+        [JsonProperty("email_timestamp")]
+        public string EmailTimestamp { get; set; }
+
+        [JsonProperty("alias")]
+        public string EmailAlias { get; set; }
+
+        [JsonProperty("list")]
+        public List<Email> Emails { get; set; }
+
+        [JsonProperty("count")]
+        public int EmailCount { get; set; }
+
+        [JsonProperty("stats")]
+        public Stats Stats { get; set; }
+
+        public void GetEmailAddress()
+        {
+            CookieContainer cookies = new CookieContainer();
+            string url = "https://api.guerrillamail.com/ajax.php?f=get_email_address";
+            Response response = HttpUtil.Get(url, cookies);
+            GuerrillaMail tempmail = JsonConvert.DeserializeObject<GuerrillaMail>(response.Json);
+            this.EmailAddress = tempmail.EmailAddress;
+            this.EmailAlias = tempmail.EmailAlias;
+            this.EmailTimestamp = tempmail.EmailTimestamp;
+            this.Cookies = response.Cookies;
+            this.SidToken = tempmail.SidToken;
+            tempmail = null;
+        }
+
+        public void CheckEmail()
+        {
+            string url = "https://api.guerrillamail.com/ajax.php?f=check_email&seq=0";
+            Response response = HttpUtil.Get(url, Cookies);
+            this.Cookies = response.Cookies;
+            GuerrillaMail tempmail = JsonConvert.DeserializeObject<GuerrillaMail>(response.Json);
+            this.EmailCount = tempmail.EmailCount;
+            this.Emails = tempmail.Emails;
+            this.Stats = tempmail.Stats;
+            this.SidToken = tempmail.SidToken;
+            tempmail = null;
+        }
+        public Email FetchEmail(string id)
+        {
+            string url = "https://api.guerrillamail.com/ajax.php?f=fetch_email&email_id=" + id;
+            Response response = HttpUtil.Get(url, Cookies);
+            this.Cookies = response.Cookies;
+            Email email = JsonConvert.DeserializeObject<Email>(response.Json);
+            return email;
+        }
+
+
+
+    }
+}
